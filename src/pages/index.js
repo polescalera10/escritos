65448ccde5afde67b4da19a4a58693af5c02418c
Pol <polescalera10@gmail.com>
2023-06-01 18:23:27 +0200

    new title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import { randomNumber } from "@/utils/randomNumber";
 import Head from "next/head";
@@ -6,13 +7,17 @@ export default function Home({ securityNumber }) {
   return (
     <>
       <Head>
-        <title>SinAutores.com</title>
+        <title>
+          Alza tu voz y escribe sin ataduras, creatividad sin limitaciones |
+          SinAutores.com{" "}
+        </title>
         <meta
           name="description"
           content="Libera tu creatividad de forma anónima. Comparte tus poemas, escritos o mensajes con nuestra comunidad. Conviértete en un héroe anónimo. O simplemente desahógate sobre el lienzo. Manda cartas de amor u odio, nadie te va a juzgar."
         />
       </Head>
       <Hero securityNumber={securityNumber} />
+      <Features />
     </>
   );
 }
